33401497d7bf6c63418d55554cf4602a01c811aa
Leo Xuzhang Lin <xuzhang@umass.edu>
2015-04-08 11:51:19 -0400

    Further simplify

--- js/script.js
+++ js/script.js
@@ -12,18 +12,12 @@ $(document).ready(function() {
     hideContent();
     $("#about").show();
   });
-  $("#project-l").click(function(){
-    hideContent();
-    $("#project").show();
-  });
-  $("#resume-l").click(function(){
-    hideContent();
-    $("#resume").show();
-  });
+
   $("#dot-l").click(function(){
     hideContent();
     $("#dot").show();
-  })
+  });
+
   $(".default-l").click(function(){
     hideContent();
     $("#default").show();
